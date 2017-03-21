c0a347fcd5231ca8e09623e6720affa7c7af2ba4
Lopez <michael.lopez@healthstream.com>
2017-03-21 14:27:44 -0500

    Checkin

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,13 @@
 # contents of chef-repo/cookbooks/my-wrapper-cookbook/recipes/default.rb
 node.default['iis-lb']['members'] = [
   {
-    'address' => 'hs11.southcentralus.cloudapp.azure.com',
+    'address' => 'hs2211.southcentralus.cloudapp.azure.com',
+    'weight' => 100,
+    'port' => 80,
+    'ssl_port' => 443
+  },
+  {
+    'address' => 'hs911.southcentralus.cloudapp.azure.com',
     'weight' => 100,
     'port' => 80,
     'ssl_port' => 443
